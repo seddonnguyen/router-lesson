3f17895dc0dae102c330e78b162ae1bf692b8c35
Stephanie Jones <stephanie.jones@flatiron.com>
2019-08-14 12:18:38 -0600

    Removed code for student clone

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 import Home from './components/Home'
 import About from './components/About'
 import List from './components/List'
@@ -16,10 +17,15 @@ class App extends Component {
   }
 
   render() {
+
     return (
-      <div>
-        hello
+
+      <div className="nav">
+        <Link className="link" to="/">Home</Link>
+        <Link className="link" to="/about/">About</Link>
+        <Link className="link" to="/list/">List</Link>
       </div>
+
     );
   }
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect, withRouter } from 'react-router-dom'
 
 class List extends Component {
     constructor(props) {
@@ -8,24 +9,22 @@ class List extends Component {
         }
     }
 
-
     render() {
-        // console.log("history props", this.props.history)
 
-        const animalList = this.props.list.list.map((animal, idx) => {
+        const animalList = this.props.animals.map((animal, idx) => {
             return <h1 key={ idx }>{ animal }</h1>
         })
         return (
             <div className="list">
                 { animalList }
 
-                <button>Redirect Component Button</button>
+                <button >Redirect Component Button</button>
 
-                <button>History Push Button</button>
+                <button >History Push Button</button>
             </div >
 
         );
     }
 }
 
-export default List;
+export default withRouter(List);
